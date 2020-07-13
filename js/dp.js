0beca67defe0cf178723e14306d8b0a3fd778de3
biem <bellomaryam@ymail.com>
2020-07-13 15:30:52 +0100

    challenge-wk3-dp-generator update

--- js/dp.js
+++ js/dp.js
@@ -1,5 +1,7 @@
 var canvas = document.getElementById("canvas");
+var frameCanvas = document.getElementById("frame-canvas");
 var ctx = canvas.getContext("2d");
+var framectx = frameCanvas.getContext("2d");
 // var radius = canvas.height / 2;
 // ctx.translate(radius, radius);
 // radius = radius * 0.90
@@ -20,58 +22,46 @@ ctx.fillText("Upload Image", canvas.width/2, canvas.height/2);
 var imagePreview = document.getElementById('image-preview');
 var btnClick = document.getElementById('btn-click-file');
 var setImgClick = document.getElementById('set-img');
-var inputClick = document.getElementById('imageUpload');
-
+var inputImageFile = document.getElementById('imageUpload');
+var generateClick = document.getElementById('generate');
+var inputValue = document.getElementById('name');
 
 
 btnClick.addEventListener('click', () => {
-    inputClick.click();
+    inputImageFile.click();
 });
+
+generateClick.addEventListener('click', () => {
+    var username = document.getElementById('user-name');
+    var boxNone = document.getElementById('instructions');
+    var canvasboxNone = document.getElementById('canvas-box');
+    var dpFrame = document.getElementById('frame');
+
+    username.innerHTML = inputValue.value;
+    boxNone.style.display = 'none';
+    canvasboxNone.style.display = 'none';
+    dpFrame.style.display = 'block'
+
+
+})
 
 function readImageUrl(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
             var prevImg = imagePreview.setAttribute('src', e.target.result);
-
-            // if (prevImg) {
-                
-            // }
-            // prevImg.style.display = 'none';
             setImage();
-            // ctx.fillText(" ", canvas.width/2, canvas.height/2);
-            // var img = document.getElementById("scream");
-            // ctx.drawImage(setImage, 10, 10);
-            // $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-            // $('#imagePreview').hide();
-            // $('#imagePreview').fadeIn(650);
         }
         reader.readAsDataURL(input.files[0]);
     }
 }
 
 function setImage() {
-    ctx.drawImage(imagePreview, 10, 10);
+
+    var imgWidth = inputImageFile.width * 2;
+    var imgHeight    = inputImageFile.height * 2;
+    ctx.drawImage(imagePreview, 0, 0, imgWidth, imgHeight);
+    framectx.drawImage(document.getElementById('image-frame'), 0, 0);
     
 }
 
-function drawImageCircle() {
-    // var pattern = ctx.createPattern(imagePreview, "no-repeat");
-    // ctx.strokeStyle = pattern;
-    // ctx.beginPath();
-    // ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-    // // ctx.fillStyle = pattern;
-    // ctx.stroke()
-}
-
-// setImgClick.addEventListener('click', () => {
-//     // inputClick.click();
-//     // ctx.drawImage(imagePreview, 10, 10);
-// });
-
-// inputClick.addEventListener('change', () => {
-//     readImageUrl(this)
-// })
-// $("#imageUpload").change(function () {
-//     readURL(this);
-// });
